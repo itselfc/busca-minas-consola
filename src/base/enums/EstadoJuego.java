c4ed91e52c0063ab2899163a6393e549863575d6
Itzel <itzelgpec@gmail.com>
2020-01-20 20:26:29 -0800

    Se agregaron textos con leyendas a los estados del juego.

--- src/base/enums/EstadoJuego.java
+++ src/base/enums/EstadoJuego.java
@@ -1,17 +1,15 @@
 package base.enums;
 
 public enum EstadoJuego {
-    PERDIDO(false,true,false),
-    CONTINUA(true,true,false),
-    GANADO(false,true,true);
+    PERDIDO("Partida perdida"),
+    CONTINUA("Partida en proceso"),
+    GANADO("Partida ganada");
 
-    public final boolean bandSiguienteTurno;
-    public final boolean bandTermino;
-    public final boolean bandGanado;
+    public final String valor;
 
-    EstadoJuego(boolean bandSiguienteTurno, boolean bandTermino, boolean bandGanado) {
-        this.bandSiguienteTurno = bandSiguienteTurno;
-        this.bandTermino = bandTermino;
-        this.bandGanado = bandGanado;
+
+    EstadoJuego(String valor) {
+     this.valor=valor
+     ;
     }
 }
